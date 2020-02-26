7c49071e9e3b86b973e188faec94fd42cbb4542f
Jerald Gutierrez <jerald.gutierrez@thinkconveyancing.com.au>
2020-02-26 12:11:54 +0800

    Update seeder date in educational

--- database/seeds/EducationalTableSeeder.php
+++ database/seeds/EducationalTableSeeder.php
@@ -18,7 +18,7 @@ class EducationalTableSeeder extends Seeder
                 "description"   =>  "",
                 "address"       =>  "Dalipit West, Alitagtag, Batangas",
                 "start_in"      =>  Carbon::now()->format('2001-06-01 00:00:00'),
-                "end_in"        =>  Carbon::now()->format('2017-04-01 00:00:00'),
+                "end_in"        =>  Carbon::now()->format('2007-04-01 00:00:00'),
                 "created_at"    =>  Carbon::now()->format('Y-m-d H:i:s'),
                 "updated_at"    =>  Carbon::now()->format('Y-m-d H:i:s')
             ],
