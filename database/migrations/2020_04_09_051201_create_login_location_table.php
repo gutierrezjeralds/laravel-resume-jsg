40f5ab2a95b0141b732f8bf13e7a287023c57b24
Jerald Gutierrez <gutierrez.jeralds@yahoo.com>
2020-04-09 14:06:19 +0800

    commit changes

--- database/migrations/2020_04_09_051201_create_login_location_table.php
+++ database/migrations/2020_04_09_051201_create_login_location_table.php
@@ -15,7 +15,7 @@ class CreateLoginLocationTable extends Migration
     {
         Schema::create('login_location', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->interger('user_id');
+            $table->integer('user_id');
             $table->string('address')->nullable();
             $table->string('latitude')->nullable();
             $table->string('longitude')->nullable();
